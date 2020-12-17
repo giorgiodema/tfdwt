72c0bf00b89dd8e117b350dae0408bc08c9e9db8
giorgiodema <giorgiodema@gmail.com>
2020-12-17 18:42:41 +0100

    multilevel v1

--- tfdwt.py
+++ tfdwt.py
@@ -3,17 +3,18 @@ from tensorflow.python.ops.gen_array_ops import transpose
 from wavelet import *
 import pywt
 import matplotlib.pyplot as plt
+import math
 
 
 coefficients = {
     "db4":{
         "dec":{
-            "lpf":tf.constant([-0.0105974018, 0.0328830117, 0.0308413818,-0.1870348117,-0.0279837694, 0.6308807679, 0.7148465706, 0.2303778133]),
-            "hpf":tf.constant([-0.2303778133, 0.7148465706,-0.6308807679,-0.0279837694, 0.1870348117, 0.0308413818,-0.0328830117,-0.0105974018])
+            "lpf":tf.constant([0.4829629131445341,0.8365163037378077,0.2241438680420134,-0.12940952255126034]),
+            "hpf":tf.constant([-0.12940952255126034,-0.2241438680420134,0.8365163037378077,-0.4829629131445341])
         },
         "rec":{
-            "lpf":tf.constant([ 0.7148465706,0.0328830117, -0.0279837694,-0.1870348117,0.0308413818, 0.6308807679,-0.0105974018,0.2303778133]),
-            "hpf":tf.constant([-0.2303778133,-0.0105974018,-0.6308807679,0.0308413818,0.1870348117,-0.0279837694,-0.0328830117,0.7148465706])
+            "lpf":tf.constant([0.2241438680420134,0.8365163037378077,0.4829629131445341,-0.12940952255126034]),
+            "hpf":tf.constant([-0.12940952255126034,-0.4829629131445341,0.8365163037378077,-0.2241438680420134])
 
         }
     }
@@ -23,7 +24,15 @@ coefficients = {
 def conv(x,filter):
     filter = tf.reshape(filter,(filter.shape[0],1,1,1))
     x = tf.reshape(x,(x.shape[0],x.shape[1],x.shape[2],1))
+    #padding = filter.shape[0]-2
+    #pad = x[:,0:padding,:]#tf.repeat(x[:,-1:,:],repeats=[padding],axis=1)
+    #x = tf.concat([x,pad],axis=1)
+    padding=filter.shape[0]
+    lpad = x[:,0:padding,:]
+    rpad = x[:,-padding:,:]
+    x = tf.concat([lpad,x,rpad],axis=1)
     o = tf.nn.conv2d(x,filter,(2,1),'SAME')
+    o = o[:,padding//2:-padding//2,:,:]
     o = tf.reshape(o,(o.shape[0],o.shape[1],o.shape[2]))
     return o
 
@@ -39,7 +48,7 @@ def log2(x):
     return tf.math.log(tf.cast(x,tf.float32))/tf.math.log(2.)
 
 
-def dwt(x,wavelet="db4",multilevel=False):
+def dwt(x,wavelet="db4",maxlevel=-1):
     """
     Params:
         -> x:   the input signal, with shape [BS,SEQ_LEN,N_DIM],
@@ -53,7 +62,7 @@ def dwt(x,wavelet="db4",multilevel=False):
 
     detail_coeffs = conv(x,hpf)
     approx_coeffs = conv(x,lpf)
-    if not multilevel:
+    if maxlevel==1:
         return tf.concat([approx_coeffs,detail_coeffs],axis=1)
     
     outputs = tf.TensorArray(tf.float32,size=0, dynamic_size=True,infer_shape=False,element_shape=tf.TensorShape([None,None,None]))
@@ -64,6 +73,8 @@ def dwt(x,wavelet="db4",multilevel=False):
         approx_coeffs = conv(approx_coeffs,lpf)
         outputs.write(idx,tf.transpose(detail_coeffs,[1,2,0]))
         idx = tf.add(idx,1)
+        if maxlevel>0 and idx >= maxlevel:
+            break
     outputs.write(idx,tf.transpose(approx_coeffs,[1,2,0]))
     coeffs = outputs.concat()
     coeffs = tf.reverse(coeffs,[0])
@@ -72,7 +83,7 @@ def dwt(x,wavelet="db4",multilevel=False):
     
 
 
-def idwt(coeffs,wavelet="db4",multilevel=False):
+def idwt(coeffs,wavelet="db4",maxlevel=-1):
     """
     Params:
         -> x: the coefficients of the dwt decomposition
@@ -85,15 +96,15 @@ def idwt(coeffs,wavelet="db4",multilevel=False):
     hpf = coefficients[wavelet]["rec"]["hpf"]
 
     signal_len = coeffs.shape[1]
-    current_len = lpf.shape[0] if multilevel else signal_len//2
+    current_len = lpf.shape[0] if maxlevel<0 else signal_len//2**(maxlevel)
     while current_len <= signal_len//2:
         lpin = coeffs[:,0:current_len,:]
         hpin = coeffs[:,current_len:2*current_len,:]
         interleaved = tf.transpose(interleave(
             tf.transpose(lpin,[1,2,0]),
             tf.transpose(hpin,[1,2,0])),[2,0,1])
-        lpout = conv(interleaved,lpf)
-        hpout = conv(interleaved,hpf)
+        lpout = tf.reverse(conv(tf.reverse(interleaved,axis=[1]),lpf),axis=[1])
+        hpout = tf.reverse(conv(tf.reverse(interleaved,axis=[1]),hpf),axis=[1])
         out = tf.transpose(interleave(
             tf.transpose(lpout,[1,2,0]),
             tf.transpose(hpout,[1,2,0])),[2,0,1])
@@ -114,20 +125,25 @@ def split_coeffs(coeffs,filter_length):
 
 
 if __name__=='__main__':
-    X,Y = signal([5],length=4096,overlap=True)
+    X,Y = signal([5],length=1024,overlap=True)
     plt.plot(Y)
     plt.show()
     yt = tf.reshape(tf.convert_to_tensor(Y,dtype=tf.float32),(1,Y.shape[0],1))
-    pcoeffs = dwt(yt,multilevel=True)
-    tcoeffs = pywt.dwt(Y,'db4',mode='periodization')
-    py_r = idwt(pcoeffs,multilevel=True)
+    maxlevel=4
+    pcoeffs = dwt(yt,maxlevel=maxlevel)
+    tcoeffs = pywt.wavedec(Y,'db4',mode='periodization')
+    py_r = idwt(pcoeffs,maxlevel=maxlevel)
     py_r = tf.reshape(py_r,(py_r.shape[1]))
     plt.plot(py_r.numpy())
     plt.show()
-    pcoeffs = split_coeffs(tf.reshape(pcoeffs,(4096)).numpy(),8)
+
+    """
+    pcoeffs = split_coeffs(tf.reshape(pcoeffs,(1024)).numpy(),4)
     y_r = pywt.waverec(pcoeffs,'db4',mode='periodization')
     plt.plot(y_r)
     plt.show()
+    """
+
     """
     for i in range(len(coeffs)-1):
         coeffs[-i] = np.zeros_like(coeffs[-i])
